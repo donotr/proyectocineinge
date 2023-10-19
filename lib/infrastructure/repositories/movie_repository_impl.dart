778e8a35bc755011b3142c52b27fcc1dc9381289
donotr <tr1900@ues.edu.sv>
2023-10-19 09:06:22 -0600

    barra busqueda cine funcional

--- lib/infrastructure/repositories/movie_repository_impl.dart
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -34,5 +34,9 @@ class MovieRepositoryImpl extends MoviesRepository {
     return datasource.getMovieById(id);
   }
 
+  @override
+  Future<List<Movie>> searchMovies (String query){
+    return datasource.searchMovies(query);
+  }
 
 }
